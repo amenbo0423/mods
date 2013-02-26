9c179b91064a5784badd91c7ffa06a8df5ff98c4
Amenbo <amenbo0423@gmail.com>
2013-02-26 23:31:34 +0900

    Im begging

---
+++ omurice/ItemOmurice.java
@@ -0,0 +1,11 @@
+package amenbo0423.mods.omurice;
+
+import net.minecraft.item.Item;
+
+public class ItemOmurice extends Item {
+
+	public ItemOmurice(int par1) {
+		super(par1);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+}
--- omurice/Omurice.java
+++ omurice/Omurice.java
@@ -1,19 +1,48 @@
 package amenbo0423.mods.omurice;
 
-import java.util.logging.Level;
-
-import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraftforge.common.Configuration;
-import net.minecraftforge.common.Property;
-import cpw.mods.fml.common.FMLLog;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
+@Mod(
+	modid = "Omurice",
+	name = "Omurice",
+	version = "0.0.1"
+)
+@NetworkMod(
+	clientSideRequired = true,
+	serverSideRequired = false
+)
 public class Omurice {
+/*
+	@SidedProxy(
+		clientSide = "amenbo0423.mods.omurice.client.ClientProxy",
+		serverSide = "amenbo0423.mods.omurice.CommonProxy"
+	)
+	public static CommonProxy proxy;
+*/
+	public static Item itemOmurice;
 
+	@Mod.Init
+	public void init(FMLInitializationEvent event){
+		itemOmurice = (new ItemOmurice(423))
+			.setIconCoord(11, 0)
+			.setItemName("itemOmurice")
+			.setCreativeTab(CreativeTabs.tabFood);
+
+		LanguageRegistry.addName(itemOmurice, "Omurice");
+		LanguageRegistry.instance().addNameForObject(itemOmurice, "ja_JP", "オムライス");
+
+		GameRegistry.addRecipe(
+			new ItemStack(itemOmurice, 1),
+			new Object[]
+			{
+				Item.egg, Item.wheat
+			});
+	}
 }
