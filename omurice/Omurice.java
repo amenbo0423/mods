a735d4c434a7f45db73a7331bca78e5847a47006
Amenbo <amenbo0423@gmail.com>
2013-02-26 22:56:00 +0900

    I begin

---
+++ omurice/Omurice.java
@@ -0,0 +1,19 @@
+package amenbo0423.mods.omurice;
+
+import java.util.logging.Level;
+
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.Property;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class Omurice {
+
+}
